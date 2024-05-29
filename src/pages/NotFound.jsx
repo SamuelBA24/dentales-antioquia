5b9aeb53bf1c7cb9bbb96d409a2ac5afd836a453
Samuel David Blanco Arango <samuelblancoarango@gmail.com>
2024-05-29 06:43:54 -0500

    ACÁ SE CAMBIO EL ESTILO DEL NOT FOUND

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
 	return (
-		<div>
-			<h1>Esta página no existe</h1>
-			<Link to="/">Volver al inicio</Link>
+		<div className='min-h-screen flex justify-center items-center flex-col'>
+			<h1 className='text-5xl'>Esta página no existe</h1>
+			<Link to="/" className="text-blue-700 text-3xl ">Volver al inicio</Link>
 		</div>
 	);
 };
