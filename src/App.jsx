d1f62c5d0a9bda8c84f3c88577b257ea817127f8
Samuel David Blanco Arango <samuelblancoarango@gmail.com>
2024-05-28 23:39:23 -0500

    Página inicio hecha

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import PublicLayout from './layouts/PublicLayout';
 
 function App() {
 	return (
 		<>
 			<Router>
 				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="*" element={<NotFound />} />
+					<Route path="/" element={<PublicLayout />}>
+						<Route index element={<Home />} />
+						<Route path="*" element={<NotFound />} />
+					</Route>
 				</Routes>
 			</Router>
 		</>
